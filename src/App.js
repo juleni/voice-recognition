6f65980d0734908a29d806c03738098a1add037a
juleni <69912179+juleni@users.noreply.github.com>
2022-11-18 15:26:13 +0100

    Added switch for rendering body based on the specified command

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Canvas from "./components/Canvas";
+import Commands from "./components/Commands";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Pictures from "./components/Pictures";
+
 import useAlan from "./hooks/useAlan";
+import { COMMANDS } from "./utils/constants";
 
 function App() {
-  useAlan();
-  return <div className="App">Voice Rec</div>;
+  const [userCommand, setUserCommand] = useState("");
+  useAlan(setUserCommand);
+
+  useEffect(() => {
+    console.log(" App USE Effect userCommand =" + userCommand);
+    console.log(userCommand);
+  }, [userCommand]);
+
+  return (
+    <div className="app">
+      <Header />
+      <Nav />
+      {console.log(userCommand.command)}
+      YOU TOLD{" "}
+      {userCommand.command === undefined
+        ? "NOTHING"
+        : ': "' + userCommand.command + '"'}
+      {(() => {
+        switch (userCommand.command) {
+          case COMMANDS.HOME: {
+            return <Home />;
+          }
+          case COMMANDS.COMMANDS: {
+            return <Commands />;
+          }
+          case COMMANDS.PICTURES: {
+            return <Pictures />;
+          }
+          case COMMANDS.CANVAS: {
+            return <Canvas />;
+          }
+
+          default: {
+            return <Home />;
+          }
+        }
+      })()}
+    </div>
+  );
 }
 
 export default App;
