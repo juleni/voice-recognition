ef045629883bdceaf89cabf86e0368b5978b696e
juleni <69912179+juleni@users.noreply.github.com>
2022-11-20 18:17:01 +0100

    added 'show picture XX' commands

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,10 @@ export const COMMANDS = {
   PICTURES: "pictures",
   CANVAS: "canvas",
   SEARCH: "search",
+  PICTURES_JANE: "jane pictures",
+  PICTURES_EMA: "ema pictures",
+  PICTURES_LEA: "lea pictures",
+  PICTURES_BAK: "bak pictures",
+  PICTURES_JULIAN: "julian pictures",
+  PICTURES_NUFKO: "nufko pictures",
 };
