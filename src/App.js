7887000a66f2d7a00c2094a7262deeb1a75714d6
juleni <69912179+juleni@users.noreply.github.com>
2022-11-20 18:16:29 +0100

    Added Pictures components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import Canvas from "./components/Canvas";
 import Commands from "./components/Commands";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Nav from "./components/Nav";
+import Navigation from "./components/Navigation";
 import Pictures from "./components/Pictures";
+import PicturesBak from "./components/PicturesBak";
+import PicturesEma from "./components/PicturesEma";
+import PicturesJane from "./components/PicturesJane";
+import PicturesJulian from "./components/PicturesJulian";
+import PicturesLea from "./components/PicturesLea";
+import PicturesNufko from "./components/PicturesNufko";
 
 import useAlan from "./hooks/useAlan";
 import { COMMANDS } from "./utils/constants";
@@ -19,10 +25,15 @@ function App() {
     console.log(userCommand);
   }, [userCommand]);
 
+  function handleNavClick(commandParam) {
+    const userCmd = { command: commandParam };
+    setUserCommand(userCmd);
+  }
+
   return (
     <div className="app">
       <Header />
-      <Nav />
+      <Navigation handleNavClick={handleNavClick} />
       {console.log(userCommand.command)}
       YOU TOLD{" "}
       {userCommand.command === undefined
@@ -41,6 +52,24 @@ function App() {
           }
           case COMMANDS.CANVAS: {
             return <Canvas />;
+          }
+          case COMMANDS.PICTURES_EMA: {
+            return <PicturesEma />;
+          }
+          case COMMANDS.PICTURES_LEA: {
+            return <PicturesLea />;
+          }
+          case COMMANDS.PICTURES_JANE: {
+            return <PicturesJane />;
+          }
+          case COMMANDS.PICTURES_JULIAN: {
+            return <PicturesJulian />;
+          }
+          case COMMANDS.PICTURES_BAK: {
+            return <PicturesBak />;
+          }
+          case COMMANDS.PICTURES_NUFKO: {
+            return <PicturesNufko />;
           }
 
           default: {
